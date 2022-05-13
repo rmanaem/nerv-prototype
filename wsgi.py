5cdbbdd02b9d0368a0ced74578d8b53cc6795314
rmanaem <rmanaem@protonmail.ch>
2022-05-12 21:05:40 -0400

    Fixed the path

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ from nerv.app1 import test
 from flask import Flask
 
 # app = create_app(r"C:\Users\Arman\Desktop\nerv\nerv\data\exp1")
-app = test(r"C:\Users\Arman\Desktop\nerv\nerv\data", Flask(__name__))
+app = test(r"C:\Users\Arman\Desktop\nerv-prototype\nerv\data", Flask(__name__))
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
